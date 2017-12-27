53a48ea3212919fadb98a2a8bdacc3d06a22e0cd
niawjunior <pasupol@dome.cloud>
2017-12-27 17:18:46 +0700

    Create Component | NoteForm, Note

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import Note from './Note/Note';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        { id: 1, noteContent: "Note 1 here"},
+        { id: 2, noteContent: "Note 2 here"},
+      ],
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>hello </h1>
-     </div>
-
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">
+            <h1>React & Firebase To-Do List</h1>
+          </div>
+         </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id}/>
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          Footer
+        </div>
+      </div>
     );
   }
 }
+
 
 export default App;
