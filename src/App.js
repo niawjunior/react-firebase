5bdd3d4fedb6c6a3e573734adb5d9cc8b4ba9b65
niawjunior <pasupol@dome.cloud>
2017-12-28 11:54:14 +0700

    get data from firebase database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import Note from './Note/Note';
 import NoteForm from './NoteForm/NoteForm';
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.addNote = this.addNote.bind(this);
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('notes');
+
     this.state = {
-      notes: [
-        { id: 1, noteContent: "Note 1 here"},
-        { id: 2, noteContent: "Note 2 here"},
-      ],
+      notes: [],
     }
   }
 
-  addNote(note) {
-    const previousNote = this.state.notes; 
-    previousNote.push({ id: previousNote.length +1, noteContent: note});
-    this.setState({
-      notes: previousNote
+  componentWillMount() {
+    const previousNotes = this.state.notes;
+
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+      this.setState({
+        notes: previousNotes
+      })
     })
+  }
+
+  addNote(note) {
+    this.database.push().set({ noteContent: note});
   }
 
   render() {
@@ -45,6 +59,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
