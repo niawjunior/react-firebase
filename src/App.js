684dce4b654abb93428fd37fd1f508e8e681449e
niawjunior <pasupol@dome.cloud>
2017-12-28 10:42:37 +0700

    update state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
 import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.addNote = this.addNote.bind(this);
     this.state = {
       notes: [
         { id: 1, noteContent: "Note 1 here"},
         { id: 2, noteContent: "Note 2 here"},
       ],
     }
+  }
+
+  addNote(note) {
+    const previousNote = this.state.notes; 
+    previousNote.push({ id: previousNote.length +1, noteContent: note});
+    this.setState({
+      notes: previousNote
+    })
   }
 
   render() {
@@ -29,7 +39,7 @@ class App extends Component {
           }
         </div>
         <div className="notesFooter">
-          Footer
+          <NoteForm addNote={this.addNote} />
         </div>
       </div>
     );
