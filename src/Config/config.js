0dfcbebc6e9dbc51ee5f3f7ee53a204cc1542222
pasupol bunsaen <pasupol@dome.cloud>
2017-12-28 13:09:32 +0700

    :fire:

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,8 +1,8 @@
 export const DB_CONFIG = {
-    apiKey: "AIzaSyBQ0xj1YeBbe4w0UT_EMj6TplGDP7dm5n8",
-    authDomain: "note-9135a.firebaseapp.com",
-    databaseURL: "https://note-9135a.firebaseio.com",
-    projectId: "note-9135a",
-    storageBucket: "note-9135a.appspot.com",
-    messagingSenderId: "176647568820"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
