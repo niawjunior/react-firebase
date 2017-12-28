66215f7eefb006fcd752e72cfff4f5f9368fd5a9
niawjunior <pasupol@dome.cloud>
2017-12-28 12:59:39 +0700

    create button remove and handle items id

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -8,11 +8,21 @@ class Note extends Component {
 
         this.noteContent = props.noteContent;
         this.noteId = props.noteId;
+        this.handleRemoveNote = this.handleRemoveNote.bind(this);
+    }
+
+    handleRemoveNote(id) {
+        this.props.removeNote(id);
     }
 
     render(props) {
         return (
             <div className="note fade-in">
+                <span 
+                    className="closebtn" 
+                    onClick={() => this.handleRemoveNote(this.noteId)}>
+                    &times;
+                </span>
                 <p className="noteContent">{ this.noteContent } </p>
             </div>
         )
